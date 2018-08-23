fdea876ca0a124e50021e1645af64ec376fae910
Ferit Tunçer <ferit@cryptolab.net>
2018-08-23 22:15:05 +0300

    search bar

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { setJSON, getJSON, decodeIPFSHash, encodeIPFSHash } from './util/IPFS.js';
-import { Col, Form, Button, FormControl } from 'react-bootstrap';
+import { Col, Row, Form, Button, FormControl } from 'react-bootstrap';
 import Dropzone from 'react-dropzone'
 import Loader from "./Loader"
 import web3 from "web3";
-import bs58 from "bs58";
 import { timestamp, getTimestamp, getHash, getTags, getId, getAllIds, getAllHashes, getStopped, getContractAddress } from './services/ProofOfExistenceService';
-
+import SearchBar from './SearchBar';
 
 export class Dashboard extends Component {
     constructor() {
@@ -16,7 +15,9 @@ export class Dashboard extends Component {
             ipfsData: [],
             loading: false,
             contractStopped: false,
-            contractAddress: "Fetching..."
+            contractAddress: "Fetching...",
+            searchResult: [],
+            counter: 1
         }
     }
 
@@ -43,115 +44,146 @@ export class Dashboard extends Component {
     }
 
     fetchData = async () => {
-        console.log("fetchData");
-        console.log("network: " + this.props.network);
-        console.log("contract address: " + await getContractAddress());
         //first get hash from smart contract
         const contractStopped = await getStopped();
-        console.log(contractStopped);
         this.setState({contractStopped, contractAddress: await  getContractAddress()});
-        console.log("address: " + this.props.specificNetworkAddress);
         const ids = await getAllIds(this.props.specificNetworkAddress);
-        console.log("ids: " + ids);
         const hashes = await getAllHashes(this.props.specificNetworkAddress);
-        console.log("hashes: " + hashes);
         //then get data off IPFS
         const ipfsHashes = hashes.map(x => encodeIPFSHash(x));
-        console.log(ipfsHashes);
         if (Array.isArray(ipfsHashes) && ipfsHashes.length === 0) { return }
         const timestamps = (await Promise.all(hashes.map(async x => getTimestamp(x)))).map(x => x.toNumber());
-        console.log(timestamps);
         const multipleDetails = (await Promise.all(ipfsHashes.map(async x => getJSON(x))));
 
-        console.log(multipleDetails);
 
         let allDetails = [];
 
         for(const id of ids){
             allDetails.push({id: id.toNumber(), hash: hashes[id], ipfsHash: ipfsHashes[id], timestamp: timestamps[id], data: multipleDetails[id].myData});
         }
-        console.log(allDetails);
 
         this.setState({ ipfsData: allDetails, loading: false, contractStopped: contractStopped.toString() })
         console.log("fetchData ended");
 
     }
+
+    fetchDataById = async (id) => {
+        if(isNaN(id) || id == ''){
+          this.setState({searchResult: []});
+          return;
+        }
+        console.log("Search id is: " + id );
+
+        const hash = await getHash(id);
+        const ipfsHash = encodeIPFSHash(hash);
+        const timestamp = (await getTimestamp(hash)).toNumber();
+        const data = await getJSON(ipfsHash);
+        console.log("fetchDataById");
+        console.log(timestamp);
+
+        let search = [];
+        search.push({id, hash, ipfsHash, timestamp, data: data.myData});
+        console.log("search");
+        console.log(search);
+
+        this.setState({searchResult: search});
+        console.log("searchResult");
+        console.log(this.state.searchResult);
+    }
+
+
     handleMyData = (e) => {
-        console.log("handleMyData: " + e.target.value);
         this.setState({ myData: e.target.value });
-        console.log(this.state.ipfsData);
     }
 
-    ipfsItems = () => {
-        return this.state.ipfsData.map((x) =>
-          <tr key="{x.id}">
-              <th scope="col">{x.id}</th>
-              <th scope="col">{x.hash}<br/>{x.ipfsHash}</th>
-              <th scope="col">{x.data}</th>
-              <th scope="col">{new Date(Number(x.timestamp + "000")).toUTCString()}</th>
-          </tr>
-        );
+    getRandomInt = (max) => {
+      return Math.floor(Math.random() * Math.floor(max));
     }
 
+    blockchainDisplay = (payload, flag) => {
+      console.log("payload " +flag);
+      console.log(payload);
+      if(!Array.isArray(payload) || payload.length === 0)
+      {
 
-
+        return (<div>NO RESULT</div>)
+      }
+      const items = payload.map((x) =>
+        <tr key={x.id}>
+            <th scope="col">{x.id}</th>
+            <th scope="col">{x.ipfsHash}<br/>{x.hash}</th>
+            <th scope="col">{x.data}</th>
+            <th scope="col">{new Date(Number(x.timestamp + "000")).toUTCString()}</th>
+        </tr>
+      );
+      return(
+          <table key={this.getRandomInt(100000)} className="table">
+            <thead>
+              <tr>
+                <th scope="col">Record ID</th>
+                <th scope="col">IPFS Hash (Plaintext / Encoded)</th>
+                <th scope="col">Data</th>
+                <th scope="col">Timestamp</th>
+              </tr>
+            </thead>
+          <tbody>
+            {items}
+          </tbody>
+        </table>
+      )
+    }
 
     render() {
         return (
-            <React.Fragment>
+            <div>
+              <React.Fragment>
+                <SearchBar onSearchTermChange={term => this.fetchDataById(term)}>
+                </SearchBar>
+                <Col sm={12}>
+                  {this.blockchainDisplay(this.state.searchResult)}
+                </Col>
+              </React.Fragment>
+
+              <React.Fragment>
                 <Col sm={12} >
                     <p> Network: {this.props.network} </p>
                     <p> Contract: {this.state.contractAddress}</p>
                     <p> Account: {this.props.specificNetworkAddress}</p>
                     <br/>
                     {this.state.ipfsData.length !== 0 ?
-                        <p>Data loaded from IPFS  </p>
+                        <h4>My Records</h4>
                         :
                         <div><h4>No record found for this account.</h4><p>Please enter and submit data on the right</p></div>
                     }
-                    <div className="blockchain-display">
-                      <table className="table">
-                        <thead>
-                          <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Hash (Contract / IPFS)</th>
-                            <th scope="col">Data</th>
-                            <th scope="col">Timestamp</th>
-                          </tr>
-                        </thead>
-                      <tbody>
-                        {this.ipfsItems()}
-
-                      </tbody>
-                      </table>
-
-
+                    <div className="my-records">
+                      {this.blockchainDisplay(this.state.ipfsData, 0)}
                     </div>
 
                 </Col>
+                </React.Fragment>
 
-                <div className="row">
+                <React.Fragment>
                 <Col sm={12}>
                     <Form horizontal onSubmit={this.handleSubmit}>
-                        <p><h4>Emergency Stop: {this.state.contractStopped.toString()}</h4></p>
                         <br/>
-                        <h4>Add New Proof</h4>
+                        <p>Emergency Stop: {this.state.contractStopped.toString()}</p>
+                        <br/>
+                        <h4>Add New Proof To The Notary</h4>
 
                         <FormControl componentClass="textarea" type="text" rows="3" placeholder="Enter text here.."
                             value={this.state.myData}
                             onChange={this.handleMyData} />
                         <br />
-                        <Button disabled={this.state.contractStopped} type="submit">Timestamp!</Button>
+                        <Button type="submit">Notarize!</Button>
                     </Form>
                 </Col>
+                {this.state.loading &&
+                    <Loader />
+                }
 
-              </div>
-              {this.state.loading &&
-                  <Loader />
-              }
+              </React.Fragment>
+            </div>
 
-
-            </React.Fragment>
         )
     }
 }
