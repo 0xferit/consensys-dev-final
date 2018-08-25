d9c425e820ea3f7b9ad7f89536bcb695e19700ad
Ferit Tunçer <ferit@cryptolab.net>
2018-08-25 17:06:22 +0300

    functionally final

--- src/ethereum/contracts/ProofOfExistence.sol
+++ src/ethereum/contracts/ProofOfExistence.sol
@@ -2,8 +2,14 @@ pragma solidity ^0.4.24;
 
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
+/**
+ * @title A proof of existence contract that stores decoded IPFS path and tags along with a timestamp as immutable records.
+ */
 contract ProofOfExistence is Ownable{
 
+    /**
+     * @dev Sets the caller as the owner of the contract.
+     */
     constructor ()
     {
         owner = msg.sender;
@@ -13,12 +19,12 @@ contract ProofOfExistence is Ownable{
     bool public stopped = false;
 
     modifier onlyOwner {
-        require(owner == msg.sender, "msg.sender should be the owner.");
+        require(owner == msg.sender, "Caller should be the owner.");
         _;
     }
 
     modifier onlyWhenNotStopped {
-        require(stopped == false, "Cant run when stopped.");
+        require(stopped == false, "Emergency stop. Cant execute this now. Restore from emergency to continue.");
         _;
     }
 
@@ -29,6 +35,11 @@ contract ProofOfExistence is Ownable{
     mapping(bytes32 => string) public hashToTags;
     mapping(address => uint[]) public userToIds;
 
+    /**
+     * @dev Timestamps and saves to registry. Records are immutable thus they can be proofs of existence.
+     * @param _hash Decoded IPFS path. To btain the original IPFS path add "1220" as a prefix and encode to base 58.
+     * @param _tags Tags as additional details to content.
+     */
     function timestamp(bytes32 _hash, string _tags) onlyWhenNotStopped public returns (uint) {
         uint previousTimestamp = hashToTimestamp[_hash];
         require (previousTimestamp == 0, 'This hash is already timestamped.');
@@ -42,14 +53,24 @@ contract ProofOfExistence is Ownable{
         return id;
     }
 
+    /**
+     * @dev Getter function for retrieving all IDs that belongs to given user at once.
+     * @param user address for querying IDs
+     */
     function getAllIds(address user) public constant returns (uint256[]) {
         return userToIds[user];
     }
 
+    /**
+     * @dev Stopping function of the Emergency Stop pattern. Disables the timestamp function.
+     */
     function emergencyStop() onlyOwner public {
         stopped = true;
     }
 
+    /**
+     * @dev Restoring function of the Emergency Stop pattern. Enables the timestamp function.
+     */
     function restoreFromEmergency() onlyOwner public {
         stopped = false;
     }
